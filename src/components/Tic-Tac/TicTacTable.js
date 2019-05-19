64d6026abf32cbb4699780eae03d89cae199e6d4
CatalinaSorina <gherzan.catalina@yahoo.com>
2019-05-19 12:20:45 +0300

    ready to play

--- src/components/Tic-Tac/TicTacTable.js
+++ src/components/Tic-Tac/TicTacTable.js
@@ -7,12 +7,12 @@ class TicTacTable extends React.Component {
     constructor(){
         super();
         this.state={
-            player:"x"
+            player:"x",
+            buttons:[]
         };
     }
 
     // componentDidMount(){
-    //     this.checkWin();
     // }
 
     play = (e) => {
@@ -28,6 +28,8 @@ class TicTacTable extends React.Component {
     }
 
     checkWin = (e) => {
+        // debugger;
+
         const col1A=document.querySelector("#col1A");
         const col1B=document.querySelector("#col1B");
         const col1C=document.querySelector("#col1C");
@@ -41,45 +43,63 @@ class TicTacTable extends React.Component {
         const winDiv=document.querySelector("#win");
         const playground=document.querySelector("#playground");
 
-        let playerClick=e.textContent;
+        console.log(this.state.player);
+        winDiv.textContent=`${this.state.player.toUpperCase()} WIN`;
 
-        if(playerClick!==" "){
-            if(col1A.textContent===col2A.textContent){
-                if(col1A.textContent===col3A.textContent){
-                    winDiv.classList.remove("none");
-                    playground.classList.add("none");
-                    console.log(col1A.textContent+" win");
-                }
-            }else if(col1A.textContent===col1B.textContent){
-                if(col1A.textContent===col1C.textContent){
-                    winDiv.classList.remove("none");
-                    playground.classList.add("none");
-                    console.log(col1A.textContent+" win");
-                }
-            }else if(col3A.textContent===col3B.textContent){
-                if(col3A.textContent===col3C.textContent){
-                    winDiv.classList.remove("none");
-                    playground.classList.add("none");
-                    console.log(col1A.textContent+" win");
-                }
-            }else if(col3A.textContent===col2B.textContent){
-                if(col3A.textContent===col1C.textContent){
-                    winDiv.classList.remove("none");
-                    playground.classList.add("none");
-                    console.log(col1A.textContent+" win");
-                }
-            }
-        }else if(col3A.textContent!==" "){
-            
-        }else if(col3A.textContent!==" "){
-
+        //===rowA===\\
+        if((col1A.textContent===col2A.textContent) && (col1A.textContent===col3A.textContent) && (col1A.textContent!==" ") && (col2A.textContent!==" ") && (col3A.textContent!==" ")){
+            winDiv.classList.remove("none");
+            playground.classList.add("none");
+            console.log(this.state.player+" win");
+        }
+        //===rowB===\\
+        else if((col1B.textContent===col2B.textContent) && (col1B.textContent===col3B.textContent) && (col1B.textContent!==" ") && (col2B.textContent!==" ") && (col3B.textContent!==" ")){
+            winDiv.classList.remove("none");
+            playground.classList.add("none");
+            console.log(this.state.player+" win");
+        }
+        //===rowB===\\
+        else if((col1C.textContent===col2C.textContent) && (col1C.textContent===col3C.textContent) && (col1C.textContent!==" ") && (col2C.textContent!==" ") && (col3C.textContent!==" ")){
+            winDiv.classList.remove("none");
+            playground.classList.add("none");
+            console.log(this.state.player+" win");
+        }
+        //===col1==\\
+        else if((col1A.textContent===col1B.textContent) && (col1A.textContent===col1C.textContent) && (col1A.textContent!==" ") && (col1B.textContent!==" ") && (col1C.textContent!==" ")){
+            winDiv.classList.remove("none");
+            playground.classList.add("none");
+            console.log(this.state.player+" win");
+        }
+        //===col2==\\
+        else if((col2A.textContent===col2B.textContent) && (col2A.textContent===col2C.textContent) && (col2A.textContent!==" ") && (col2B.textContent!==" ") && (col2C.textContent!==" ")){
+            winDiv.classList.remove("none");
+            playground.classList.add("none");
+            console.log(this.state.player+" win");
+        }
+        //===col3==\\
+        else if((col3A.textContent===col3B.textContent) && (col3A.textContent===col3C.textContent) && (col3A.textContent!==" ") && (col3B.textContent!==" ") && (col3C.textContent!==" ")){
+            winDiv.classList.remove("none");
+            playground.classList.add("none");
+            console.log(this.state.player+" win");
+        }
+        //===diagonala end===\\
+        else if((col3A.textContent===col2B.textContent) && (col3A.textContent===col1C.textContent) && (col3A.textContent!==" ") && (col2B.textContent!==" ") && (col1C.textContent!==" ")){
+            winDiv.classList.remove("none");
+            playground.classList.add("none");
+            console.log(this.state.player+" win");
+        }
+        //===diagonala begin===\\
+        else if((col1A.textContent===col2B.textContent) && (col1A.textContent===col3C.textContent) && (col1A.textContent!==" ") && (col2B.textContent!==" ") && (col3C.textContent!==" ")){
+            winDiv.classList.remove("none");
+            playground.classList.add("none");
+            console.log(this.state.player+" win");
         }
     }
 
     render() {
         return (
             <div className="table">
-                <div id="win" className="win none">{this.state.player.toUpperCase()} WIN</div>
+                <div id="win" className="win none">WIN</div>
                 <div id="playground">
                     <TableRow playerId="A" play={this.play}/>
                     <TableRow playerId="B" play={this.play}/>
