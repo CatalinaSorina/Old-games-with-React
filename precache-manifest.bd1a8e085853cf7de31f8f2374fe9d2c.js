c963b691ee3c24cdde052b9bbdd097a82ba3f508
CatalinaSorina <gherzan.catalina@yahoo.com>
2019-05-27 21:10:48 +0300

    Updates

---
+++ precache-manifest.bd1a8e085853cf7de31f8f2374fe9d2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaeac3a5bbbc9dd4d2d47a6536c5e064",
+    "url": "/Old-games-with-React/index.html"
+  },
+  {
+    "revision": "fd8c544c6250b639de00",
+    "url": "/Old-games-with-React/static/css/main.d1791d59.chunk.css"
+  },
+  {
+    "revision": "401b26058173da2ef6ef",
+    "url": "/Old-games-with-React/static/js/2.bb250f8b.chunk.js"
+  },
+  {
+    "revision": "fd8c544c6250b639de00",
+    "url": "/Old-games-with-React/static/js/main.b0a7c7c8.chunk.js"
+  },
+  {
+    "revision": "3f35132a79adf4122502",
+    "url": "/Old-games-with-React/static/js/runtime~main.9f404777.js"
+  }
+]);
